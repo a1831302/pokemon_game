6fdd41b3d72f5b7b463308c16dedd1f541aa0d65
a1831302 <a1831302@adelaide.edu.au>
2024-10-03 14:42:21 +0930

    attack add

---
+++ Attack.h
@@ -0,0 +1,38 @@
+#ifndef ATTACK_H
+#define ATTACK_H
+
+#include <iostream>
+#include <string>
+
+#include "Pokemon.h"
+
+//Attack Robot class child class of Robot
+class Attack : public Pokemon{
+public:
+
+    int sig_damage;
+    int type_damage;
+    int quick_attack;
+
+    int num_hits;
+
+    bool hit_success;
+
+    //constructor
+    Attack();
+    Attack(int attack_used);
+
+    bool get_success();
+
+    int get_attack();
+
+    int get_attackType();
+
+    int set_numHits(int num_hits);
+    int get_numHits();
+
+    //HP drain function
+    void HP_drain(int attack_stat);
+};
+
+#endif
