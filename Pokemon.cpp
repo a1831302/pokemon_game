b242234bfe3e9433cfed78fab825b2b058d04a27
Kevin <a1831302@adelaide.edu.au>
2024-10-03 14:56:05 +0930

    update

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <string>
+#include <algorithm> // for std::fill
 #include "Pokemon.h"
-
 
 using namespace std;
 
 // strengths: Fire = 1, Water = 2, Ground = 3, Grass = 4, Normal = 5, Electric = 6, Psychic = 7, Fairy = 8, Fighting = 9
 
-
 // constructor
-Pokemon::Pokemon(int new_PokemonID){
+Pokemon::Pokemon(int new_PokemonID) {
+    // Initialize strengths and weaknesses arrays with zeros
+    fill(begin(strengths), end(strengths), 0);
+    fill(begin(weaknesses), end(weaknesses), 0);
+    
     switch(new_PokemonID) {
         case 1:
             name = "Squirtle";
             HP = 200;
             attack = "Water Gun";
             sig_attack = "Hydro Pump";
-            strengths = {1, 0, 0}; //Fire 
+            strengths[0] = 1; // Fire
             stringStrengths = "Fire";
-            weaknesses[2] = {6, 4}; //electric and grass
+            weaknesses[0] = 6; // Electric
+            weaknesses[1] = 4; // Grass
             stringWeaknesses = "Electric, Grass";
             break;
         case 2:
@@ -26,9 +30,10 @@ Pokemon::Pokemon(int new_PokemonID){
             HP = 200;
             attack = "Vine Whip";
             sig_attack = "Solar Beam";
-            strengths = {2, 3, 0}; 
+            strengths[0] = 2; // Water
+            strengths[1] = 3; // Ground
             stringStrengths = "Water, Ground";
-            weaknesses = {1, 0}; 
+            weaknesses[0] = 1; // Fire
             stringWeaknesses = "Fire";
             break;
         case 3:
@@ -36,9 +41,10 @@ Pokemon::Pokemon(int new_PokemonID){
             HP = 200;
             attack = "Ember";
             sig_attack = "Flamethrower";
-            strengths = {4,0,0}; 
+            strengths[0] = 4; // Grass
             stringStrengths = "Grass";
-            weaknesses = {3,2}; 
+            weaknesses[0] = 3; // Ground
+            weaknesses[1] = 2; // Water
             stringWeaknesses = "Ground, Water";
             break;
         case 4:
@@ -46,19 +52,20 @@ Pokemon::Pokemon(int new_PokemonID){
             HP = 200;
             attack = "Thunder Shock";
             sig_attack = "Thunder";
-            strengths = {2,0,0}; 
+            strengths[0] = 2; // Water
             stringStrengths = "Water";
-            weaknesses = {3,0}; 
+            weaknesses[0] = 3; // Ground
             stringWeaknesses = "Ground";
             break;
         case 5:
             name = "Machop";
             HP = 400;
             attack = "Karate Chop";
-            sig_attack = "Sesimic Toss";
-            strengths = {5,0,0}; 
+            sig_attack = "Seismic Toss";
+            strengths[0] = 5; // Normal
             stringStrengths = "Normal";
-            weaknesses = {7,8}; 
+            weaknesses[0] = 7; // Psychic
+            weaknesses[1] = 8; // Fairy
             stringWeaknesses = "Psychic, Fairy";
             break;
         case 6:
@@ -66,9 +73,8 @@ Pokemon::Pokemon(int new_PokemonID){
             HP = 400;
             attack = "Tail Whip";
             sig_attack = "Super Fang";
-            strengths = {0,0,0}; 
             stringStrengths = "No Strengths";
-            weaknesses = {9,0}; 
+            weaknesses[0] = 9; // Fighting
             stringWeaknesses = "Fighting";
             break;
         case 7:
@@ -76,9 +82,11 @@ Pokemon::Pokemon(int new_PokemonID){
             HP = 220;
             attack = "Sand Attack";
             sig_attack = "Fury Swipes";
-            strengths = {6,1,0}; 
+            strengths[0] = 6; // Electric
+            strengths[1] = 1; // Fire
             stringStrengths = "Electric, Fire";
-            weaknesses = {2,4}; 
+            weaknesses[0] = 2; // Water
+            weaknesses[1] = 4; // Grass
             stringWeaknesses = "Water, Grass";
             break;   
         case 8:  
@@ -86,50 +94,53 @@ Pokemon::Pokemon(int new_PokemonID){
             HP = 1000;
             attack = "Slam";
             sig_attack = "Dragon Rage";
-            strengths = {4,1,2}; 
+            strengths[0] = 4; // Grass
+            strengths[1] = 1; // Fire
+            strengths[2] = 2; // Water
             stringStrengths = "Grass, Fire, Water";
-            weaknesses = {7,8}; 
-            stringWeaknesses = "No Weaknesses";
+            weaknesses[0] = 7; // Psychic
+            weaknesses[1] = 8; // Fairy
+            stringWeaknesses = "Psychic, Fairy";
             break; 
         default:
-            name = "No Name";
+            name = "";
             HP = 0;
-            attack = "No Attack";
-            sig_attack = "No Signature Attack";
-            strengths = {0,0,0};
-            stringStrengths = "No Strengths";
-            weaknesses = {0,0};
-            stringWeaknesses = "No Weaknesses";
+            attack = "";
+            sig_attack = "";
+            stringStrengths = "";
+            stringWeaknesses = "";
             break;
     }
-};
+}
 
-//default constructor
-Pokemon::Pokemon(){
+// default constructor
+Pokemon::Pokemon() {
     name = "No Name";
     HP = 0;
     attack = "No Attack";
-    strengths = [0,0,0];
+    sig_attack = "No Signature Attack";
+    fill(begin(strengths), end(strengths), 0);
     stringStrengths = "No Strengths";
-    weaknesses = [0,0];
+    fill(begin(weaknesses), end(weaknesses), 0);
     stringWeaknesses = "No Weaknesses";
-};
+}
 
-
-string Pokemon::get_Pokemon_name(){
+// Get the Pokemon's name
+string Pokemon::get_Pokemon_name() {
     return name;
-};
+}
 
-void Pokemon::set_Pokemon_HP(int new_HP){
+// Set the Pokemon's HP
+void Pokemon::set_Pokemon_HP(int new_HP) {
     HP = new_HP;
-    };
+}
 
-int Pokemon::get_Pokemon_HP(){
+// Get the Pokemon's HP
+int Pokemon::get_Pokemon_HP() {
     return HP;
-};
+}
 
-
-
+// Print Pokemon info
 void Pokemon::print_Pokemon_info(int PokemonID) {
     cout << "Name: " << name << "\n";
     cout << "HP: " << HP << "\n";
@@ -137,7 +148,4 @@ void Pokemon::print_Pokemon_info(int PokemonID) {
     cout << "SignatureMove: " << sig_attack << "\n";
     cout << "Strengths: " << stringStrengths << "\n";
     cout << "Weaknesses: " << stringWeaknesses << "\n";
-
-};
-
-
+}
--- Pokemon.h
+++ Pokemon.h
@@ -14,21 +14,18 @@ class Pokemon {
         int HP;
         string attack;
         string sig_attack;
-        int strengths[3];
+        int strengths[4]; // maximum of 4 strengths
         string stringStrengths;
-        int weaknesses[2];
-        string stringWeaknesses; //for printing out
+        int weaknesses[3]; // maximum of 3 weaknesses
+        string stringWeaknesses; // for printing out
         
         Pokemon();
         Pokemon(int new_PokemonID);
 
         void print_Pokemon_info(int PokemonID);
-
-
         void set_Pokemon_HP(int new_HP);
         int get_Pokemon_HP();
-
-
+        string get_Pokemon_name();
 };
 
 #endif
