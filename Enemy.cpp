34143303ba6c638001cbcc5023a8aafff748709f
a1860278 <a1860278@adelaide.edu.au>
2024-10-04 11:42:35 +0930

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 // default constructor
 Enemy::Enemy(){
-    opponent_pokemon = Pokemon();
+    enemy_pokemon = Pokemon();
 }
 
 // constructor
@@ -17,11 +17,11 @@ Enemy::Enemy(Pokemon new_enemy_pokemon){
 void Enemy::set_pokemon(Pokemon new_enemy_pokemon){enemy_pokemon = new_enemy_pokemon;}
 
 // used to get the pokemon the Enemy has
-Robot Enemy::get_pokemon(){
-    return opp_pokemon;
+Pokemon Enemy::get_pokemon(){
+    return enemy_pokemon;
 }
 
-//  opponents implementation of the Battler classes pure virtual function
+//  enemy implementation of the Battler classes pure virtual function
 void Enemy::set_name(){
     bool valid_choice = 0; //to show error if player doesn't pick valid choice
     cout << "Which AI do you want to verse first?" << endl;
@@ -32,7 +32,7 @@ void Enemy::set_name(){
     int choice;
     cin >> choice;
     
-    // sets the opponents name to the chosen AI name
+    // sets the enemy name to the chosen AI name
     if(choice == 1){
         name = "Sachin";
         valid_choice = 1;
