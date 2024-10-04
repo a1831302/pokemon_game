6590aa6e010e938dda975522ac84985dd77fba45
a1831302 <a1831302@adelaide.edu.au>
2024-10-04 11:35:02 +0930

    add

--- Multiplier.h
+++ Multiplier.h
@@ -1,1 +1,31 @@
 
+#ifndef MULTIPLIER_H
+#define MULTIPLIER_H
+
+#include <iostream>
+#include <string>
+
+#include "Pokemon.h"
+
+//Attack Robot class child class of Pokemon
+class Attack : public Pokemon{
+public:
+
+    int attack_used;
+    int damage;
+    int attack_type;
+
+    int num_hits;
+
+    //constructor
+    Attack();
+    Attack(int attack_used);
+
+    int set_numHits(int num_hits);
+    int get_numHits();
+
+    //HP drain function
+    int HP_drain(int hp_deduct);
+};
+
+#endif
