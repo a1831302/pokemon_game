c9785ccee3aa646253106a825bdc6ea71d55a43f
a1860278 <a1860278@adelaide.edu.au>
2024-10-05 11:27:49 +0930

    Create SigAttack.h

---
+++ SigAttack.h
@@ -0,0 +1,22 @@
+#ifndef SIGATTACK_H
+#define SIGATTACK_H
+
+#include <iostream>
+#include <string>
+
+#include "Pokemon.h"
+#include "Attack.h"
+
+//Signature attack class child class of Attack
+class SigAttack : public Attack{
+public:
+    //constructors
+    SigAttack();
+    sigAttack(int num_hits);
+
+    
+
+
+};
+
+#endif
