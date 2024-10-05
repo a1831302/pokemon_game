ba2c1a372b8175c44132f5d494fc7db923aecb4f
a1860278 <a1860278@adelaide.edu.au>
2024-10-05 11:26:01 +0930

    Create QuickAttack.h

---
+++ QuickAttack.h
@@ -0,0 +1,21 @@
+#ifndef QUICKATTACK_H
+#define QUICKATTACK_H
+
+#include <iostream>
+#include <string>
+
+#include "Pokemon.h"
+#include "Attack.h"
+
+//Quick attack class child class of Attack
+class QuickAttack : public Attack{
+public:
+    //constructor
+    QuickAttack();
+
+    
+
+
+};
+
+#endif
