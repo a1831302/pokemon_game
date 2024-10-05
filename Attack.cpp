a6ec1633c58f909bb0ebbd29080ec7980724f141
a1860278 <a1860278@adelaide.edu.au>
2024-10-05 11:32:48 +0930

    Create Attack.cpp

---
+++ Attack.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm> // for std::fill
+#include "Pokemon.h"
+#include "Attack.h"
+#include <random>
+
+using namespace std;
+
+Attack::Attack() {
+    //empty default constructor
+}
+
+int Attack::success_rate() {
+    // Initialize a random number engine
+    std::random_device dev;  // Seed generator (for true random seed)
+    std::mt19937 rng(dev()); // Mersenne Twister random number engine
+
+    // Define the distribution range (e.g., between 1 and 100)
+    std::uniform_int_distribution<std::mt19937::result_type> dist(1, 100);
+
+    // Generate a random number
+    int chance = dist(rng);
+    return chance;
+}
+
+//still to do
+int Attack::HP_drain(int hp_deduct) {
+
+}
+
+
+
+
+
+
