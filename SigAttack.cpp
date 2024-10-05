37b1fe5bf0bd626b9ecb7a57fa19e90d9ebdfe35
a1860278 <a1860278@adelaide.edu.au>
2024-10-05 11:28:07 +0930

    Create SigAttack.cpp

---
+++ SigAttack.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm> // for std::fill
+#include "Pokemon.h"
+#include "Attack.h"
+#include "SigAttack.h"
+#include <random>
+
+using namespace std;
+
+SigAttack::SigAttack() {
+    //empty
+}
+
+SigAttack::SigAttack(num_hits) {
+    
+        if (num_hits > 4){ //5 hits
+            attack_type = pokemon_type; // pokemon type
+            damage = 70;
+            num_hits = 0;
+            cout << name << " used signature attack!" << endl;
+        } else {
+            cout << "Signature is not ready. Try Again: " << endl;
+        }
+}
+
+
+
+
+
+
+
+
