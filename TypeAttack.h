6bc381948e0dbd69c8ff0e44e396532f6a68b68c
a1860278 <a1860278@adelaide.edu.au>
2024-10-05 11:26:37 +0930

    Create TypeAttack.h

---
+++ TypeAttack.h
@@ -0,0 +1,21 @@
+#ifndef TYPEATTACK_H
+#define TYPEATTACK_H
+
+#include <iostream>
+#include <string>
+
+#include "Pokemon.h"
+#include "Attack.h"
+
+//Type attack class child class of Attack
+class TypeAttack : public Attack{
+public:
+    //constructor
+    TypeAttack();
+
+    
+
+
+};
+
+#endif
