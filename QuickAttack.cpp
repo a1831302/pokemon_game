40d196fa9fd25d303a4586332c3414a1fcd79e54
a1860278 <a1860278@adelaide.edu.au>
2024-10-05 11:26:18 +0930

    Create QuickAttack.cpp

---
+++ QuickAttack.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm> // for std::fill
+#include "Pokemon.h"
+#include "Attack.h"
+#include "QuickAttack.h"
+#include <random>
+
+using namespace std;
+
+QuickAttack::QuickAttack() {
+    cout << name << " used quick attack!" << endl;
+        if (chance < 80){ //20% chance of missing
+            attack_type = 5; // normal type
+            damage = 10;
+            num_hits++;
+        } else {
+            cout << "Quick Attack missed!" << endl;
+            damage = 0;
+        }
+}
+
+
+
+
+
+
+
