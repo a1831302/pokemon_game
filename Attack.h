bcad62e3fe072566df33f3370f023d90c627b5c9
a1860278 <a1860278@adelaide.edu.au>
2024-10-05 11:31:57 +0930

    Create Attack.h

---
+++ Attack.h
@@ -0,0 +1,35 @@
+#ifndef ATTACK_H
+#define ATTACK_H
+
+#include <iostream>
+#include <string>
+
+#include "Pokemon.h"
+
+//Attack Pokemon class child class of Pokemon
+class Attack : public Pokemon{
+public:
+    //int attack_used;
+    int damage;
+    int attack_type;
+    //bool hit_success;
+    int num_hits;
+    int chance;
+
+    //returns value (chance) between 1 and 100 to use for quick and type attacks
+    int success_rate();
+
+    //constructor
+    Attack(); 
+    //Attack(int attack_used);
+
+    //bool hit_success(int attack_used, bool hit_success);
+
+    //int set_numHits(int num_hits);
+    //int get_numHits();
+
+    //HP drain function
+    int HP_drain(int hp_deduct);
+};
+
+#endif
