227c3b4ebfd62d6a38bf1ab08a9c1129bee22fe7
a1860278 <a1860278@adelaide.edu.au>
2024-10-05 11:26:53 +0930

    Create TypeAttack.cpp

---
+++ TypeAttack.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm> // for std::fill
+#include "Pokemon.h"
+#include "Attack.h"
+#include "TypeAttack.h"
+#include <random>
+
+using namespace std;
+
+TypeAttack::TypeAttack() {
+    cout << name << " used type attack!" << endl;
+        if (chance < 50){ //50% chance of missing
+            attack_type = pokemon_type; // pokemon type
+            damage = 30;
+            num_hits++;
+        } else {
+            cout << "Type Attack missed!" << endl;
+            damage = 0;
+        }
+}
+
+
+
+
+
+
+
+
