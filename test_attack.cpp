d9839d2db8f7fa1a574362d4e2a0049e3bb0e668
a1831302 <a1831302@adelaide.edu.au>
2024-10-06 15:00:08 +1030

    add new test file

--- test_attack.cpp
+++ test_attack.cpp
@@ -1,43 +1,46 @@
 #include <iostream>
 #include "Pokemon.h"
+#include "QuickAttack.h"
+#include "TypeAttack.h"
+#include "SigAttack.h"
 #include "Attack.h"
 
 using namespace std;
 
 int main() {
-    Pokemon p(1); 
+    // Create a Pokémon
+    Pokemon pikachu;
+    pikachu.name = "Pikachu";
+    pikachu.pokemon_type = 6;  // Electric type for example
+    pikachu.HP = 200;
 
-    cout << "Testing with Pokémon: " << p.get_Pokemon_name() << "\n";
-    p.print_Pokemon_info(1);
-    
-    // Create an Attack object
-    Attack h;
+    // Print initial Pokémon info
+    cout << "Starting Test for " << pikachu.name << " (HP: " << pikachu.HP << ")" << endl;
 
-    // Test Quick Attack (attack_used = 1)
-    cout << "\nPerforming Quick Attack (Attack type 1): \n";
-    h = Attack(1);  // Using attack type 1
-    cout << "Damage dealt: " << h.damage << "\n";
-    cout << "Number of successful hits: " << h.get_numHits() << "\n";
+    // Test Quick Attack
+    cout << "\n---Testing Quick Attack---" << endl;
+    QuickAttack quickAtk(pikachu.success_rate());  // Generates a random chance
+    pikachu.HP -= quickAtk.damage;  // Deduct damage from Pokémon's HP if hit was successful
+    cout << pikachu.name << "'s HP after Quick Attack: " << pikachu.HP << endl;
 
-    // Test Type Attack (attack_used = 2)
-    cout << "\nPerforming Type Attack (Attack type 2): \n";
-    h = Attack(2);  // Using attack type 2
-    cout << "Damage dealt: " << h.damage << "\n";
-    cout << "Number of successful hits: " << h.get_numHits() << "\n";
+    // Test Type Attack
+    cout << "\n---Testing Type Attack---" << endl;
+    TypeAttack typeAtk(pikachu.success_rate());  // Generates a random chance
+    pikachu.HP -= typeAtk.damage;  // Deduct damage from Pokémon's HP if hit was successful
+    cout << pikachu.name << "'s HP after Type Attack: " << pikachu.HP << endl;
 
-    // Simulate several attacks to build up hits
-    for (int i = 0; i < 5; ++i) {
-        cout << "\nPerforming attack #" << i + 1 << ": \n";
-        h = Attack(1);  // Keep using attack type 2
-        cout << "Damage dealt: " << h.damage << "\n";
-        cout << "Number of successful hits: " << h.get_numHits() << "\n";
-    }
+    // Simulate multiple hits to enable Signature Attack
+    cout << "\nSimulating hits for Signature Attack..." << endl;
+    pikachu.num_hits = 5;  // Simulate that 5 successful hits have occurred
 
-    // Test Signature Attack (attack_used = 3)
-    cout << "\nAttempting Signature Attack (Attack type 3): \n";
-    h = Attack(3);  // Using attack type 3
-    cout << "Damage dealt: " << h.damage << "\n";
-    cout << "Number of successful hits (should reset): " << h.get_numHits() << "\n";
+    // Test Signature Attack
+    cout << "\n---Testing Signature Attack---" << endl;
+    SigAttack sigAtk(pikachu.num_hits);  // Signature Attack should be ready after 5 hits
+    pikachu.HP -= sigAtk.damage;  // Deduct damage from Pokémon's HP if signature attack hits
+    cout << pikachu.name << "'s HP after Signature Attack: " << pikachu.HP << endl;
+
+    // Print final Pokémon info
+    cout << "\nFinal HP of " << pikachu.name << ": " << pikachu.HP << endl;
 
     return 0;
 }
