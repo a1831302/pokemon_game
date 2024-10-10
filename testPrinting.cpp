e1d8b215068a0cad8ef385be85b3e48d683c3d4a
a1860278 <a1860278@adelaide.edu.au>
2024-10-10 15:58:32 +1030

    Create testPrinting.cpp

---
+++ testPrinting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include "Pokemon.h"
+
+int main() {
+
+    Pokemon Pok1 = Pokemon(1);
+    Pokemon Pok2 = Pokemon(2);
+    Pokemon Pok3 = Pokemon(3);
+
+    int HP1 = Pok1.get_Pokemon_HP();
+    cout << HP1 << endl;
+    Pok1.set_Pokemon_HP(250);
+    int HP2 = Pok1.get_Pokemon_HP();
+    cout << HP2 << endl;
+    int hits = Pok1.get_num_hits();
+    cout << hits << endl;
+    Pok1.HP_drain(24);
+    int HP3 = Pok1.get_Pokemon_HP();
+    cout << HP3 << endl;
+
+
+    return 0;
+}
+
+
+
+
+
+
