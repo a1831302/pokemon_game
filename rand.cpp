e9ffec3f1e6f392dc4830a92ad2d9cd1395a9280
a1860278 <a1860278@adelaide.edu.au>
2024-10-10 16:23:02 +1030

    Update rand.cpp

--- rand.cpp
+++ rand.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>   // Required for random number generation
 
+
 int rand(int min, int max) {
     // Initialize a random number engine
     std::random_device dev;  
@@ -14,3 +15,27 @@ int rand(int min, int max) {
 
     return random;
 }
+
+
+
+//other random number generator function
+
+/*
+int rand(int offset, int range) {
+    //rand number with offset and range
+    //range of 10 = 0 - 9
+    //thus, offset = 1, range = 10 -> 1-10
+    
+    int random =  offset + rand() % range;
+
+    return random;
+}
+*/
+
+
+
+
+
+
+
+
