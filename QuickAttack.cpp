524d987580a44a3c4d41453784d56d5ba97363fd
a1860278 <a1860278@adelaide.edu.au>
2024-10-13 11:18:03 +1030

    Update QuickAttack.cpp

--- QuickAttack.cpp
+++ QuickAttack.cpp
@@ -11,14 +11,14 @@ using namespace std;
 
 QuickAttack::QuickAttack() {}
 
-QuickAttack::QuickAttack(int chance) {
-    cout << name << " used quick attack!" << endl;
+QuickAttack::QuickAttack(int chance, Pokemon pok) {
+    cout << pok.name << " used quick attack!" << endl;
         if (chance < 80){ //20% chance of missing
             attack_type = 5; // normal type
             damage = 10;
-            num_hits++;
+            pok.num_hits++;
         } else {
-            cout << "Quick Attack missed!" << endl;
+            cout << pok.name << "'s quick attack missed!" << endl;
             damage = 0;
         }
 }
