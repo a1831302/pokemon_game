45f7be0763e650e75d5b0b93ae18b1265cae807e
a1860278 <a1860278@adelaide.edu.au>
2024-10-13 16:53:33 +1030

    Create testClear.cpp

---
+++ testClear.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+//#include<stdlib.h>
+using namespace std;
+
+int main() {
+
+    cout << "Hello " << endl;
+
+    system("clear");
+
+    cout << "Empty screeen" << endl;
+
+    return 0;
+}
