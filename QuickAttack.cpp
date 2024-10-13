40ad864a0fc2b0f1dbee68c3cda4616600a389ad
a1860278 <a1860278@adelaide.edu.au>
2024-10-13 12:31:33 +1030

    Update QuickAttack.cpp

--- QuickAttack.cpp
+++ QuickAttack.cpp
@@ -16,7 +16,7 @@ QuickAttack::QuickAttack(int chance, Pokemon pok) {
         if (chance < 80){ //20% chance of missing
             attack_type = 5; // normal type
             damage = 10;
-            pok.num_hits++;
+            pok.set_num_hits(pok.get_num_hits());
         } else {
             cout << pok.name << "'s quick attack missed!" << endl;
             damage = 0;
