6b9a3a3e883a2f200c304aa782b8b2e16a13c2d1
a1860278 <a1860278@adelaide.edu.au>
2024-10-13 11:29:23 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -137,12 +137,16 @@ int main(){
     //chosen attack
     if (choice == 1) {
         int attack_choice = 0;   
+        int num_attacks = 0;
         cout << "Pick an attack: " << endl;
+        if (num_attacks = 0) {
         cout << "Quick attack does 10 damage (80% success rate), Type attack does 30 damage (50% success rate), and can be multiplied. " << endl;
         cout << "Signature attack does 70 damage, but requires five successful previous attacks. Currently, you have " << player_Pok.get_num_hits() << " previous attacks. " << endl;
+        }
         cout << "Quick Attack(1), Type Attack(2), Signature Attack(3)" << endl;
+        num_attacks++;
 
-        //again work out redraw functiona d highlighted text instead of 1,2,3
+        //again work out redraw function and highlighted text instead of 1,2,3
         while(attack_choice !=1 && attack_choice != 2 && attack_choice !=3){
             cout<< "Select move: ";
             cin >> attack_choice;
