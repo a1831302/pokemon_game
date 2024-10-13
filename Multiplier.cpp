e97bd1a8dd689f7ae7cd7a15dce01c44ea9b496a
a1860278 <a1860278@adelaide.edu.au>
2024-10-13 16:06:27 +1030

    Update Multiplier.cpp

--- Multiplier.cpp
+++ Multiplier.cpp
@@ -8,9 +8,8 @@ using namespace std;
 // Default constructor
 Multiplier::Multiplier() {}
 
-// Constructor with pokemon_type
-Multiplier::Multiplier(Pokemon pok1, Pokemon pok2, int pokemon_type) {
-    pok1.multiplier = 1; // Default multiplier
+double Multiplier::find_mult(Pokemon pok1, Pokemon pok2) {
+      multiplier = 1; // Default multiplier
 
     // Get the strengths and weaknesses of the Pokemon being attacked
     int* strengths = pok2.getStrengths();
@@ -18,21 +17,26 @@ Multiplier::Multiplier(Pokemon pok1, Pokemon pok2, int pokemon_type) {
 
     // Check if the attack type matches any of the strengths (deal less damage)
     for (int i = 0; i < 3; ++i) {
-        if (strengths[i] != 0 && pokemon_type == strengths[i]) {
-            pok1.multiplier = 0.5;  // Less damage for strengths
+        if (strengths[i] != 0 && pok1.pokemon_type == strengths[i]) {
+            multiplier = 0.5;  // Less damage for strengths
             cout << "The attack type matches a strength. Multiplier is set to 0.5" << endl;
+            return multiplier;
         }
     }
 
     // Check if the attack type matches any of the weaknesses (deal more damage)
     for (int i = 0; i < 2; ++i) {
         if (weaknesses[i] != 0 && pokemon_type == weaknesses[i]) {
-            pok1.multiplier = 2;  // More damage for weaknesses
+            multiplier = 2;  // More damage for weaknesses
             cout << "The attack type matches a weakness. Multiplier is set to 2" << endl;
-            return;  // Exit the loop since we found a match
+            return multiplier;  // Exit the loop since we found a match
         }
     }
 
     // If no match is found, multiplier remains 1
+    if (multiplier == 1) {
     cout << "No strength or weakness match. Multiplier is set to 1" << endl;
+    };
+    return multiplier;
+
 }
