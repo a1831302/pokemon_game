5f704b2e444e447fe5c0b079c5bf69a1be0ef143
a1860278 <a1860278@adelaide.edu.au>
2024-10-18 13:20:51 +1030

    Update Player.h

--- Player.h
+++ Player.h
@@ -12,7 +12,7 @@ using namespace std;
 class Player : public Pokemon {
     protected:
         Pokemon player_pokemon;  // the player is a pokemon
-        medicine* medicine_bag;   // the player has a medicine bag filled with different medicines they can use
+           // the player has a medicine bag filled with different medicines they can use
         int medicine_count;   // the number of medicines in the potion bag
         
     public:
@@ -21,7 +21,7 @@ class Player : public Pokemon {
         Pokemon get_pokemon();   // used to get the pokemon the player has
         void set_pokemon(Pokemon new_pokemon);    // used to set the pokemon the player has
 
-        
+        medicine* medicine_bag;
 
         void set_medicine_bag(int num_medicines);   // creates an empty potion_bag of a certain length
         void add_medicine(int medicine_number, medicine medicine1);   // adds a medicine to the potion bag
